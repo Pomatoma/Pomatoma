b4dac2818a0bc862ede1a517fee2be44abc508b2
Ahhhhhyeong <dkdud081@gmail.com>
2025-07-28 16:46:37 +0900

    feat: navigate 연결

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,35 @@ import React from 'react';
 import Button from './Button';
 import TomatoIcon from '../assets/icons/tomato.svg'; // ReactComponent로 import
 import { useUserStore } from '../store/userStore';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
-  const { isAuthenticated } = useUserStore();
+  const { isAuthenticated, logout } = useUserStore();
+  let navigate = useNavigate();
   return (
     <div className='py-2 px-8 w-full bg-[var(--color-secondary)] flex flex-row justify-between items-center'>
-      <img src={TomatoIcon} alt='Tomato' width={50} height={45} className='inline-block' />
+      <Link to={'/'}>
+        <img src={TomatoIcon} alt='Tomato' width={50} height={45} className='inline-block' />
+      </Link>
       <div>
         {isAuthenticated ? (
-          <Button size='sm' filled='filled' disabled={false} onClick={() => {}}>
+          <Button
+            size='sm'
+            filled='filled'
+            disabled={false}
+            onClick={() => {
+              logout();
+            }}>
             로그아웃
           </Button>
         ) : (
-          <Button size='sm' filled='outline' disabled={false} onClick={() => {}}>
+          <Button
+            size='sm'
+            filled='outline'
+            disabled={false}
+            onClick={() => {
+              navigate('/auth');
+            }}>
             로그인
           </Button>
         )}
