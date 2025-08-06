407184eade433797825dd5fcabfe09c7af5f1d21
hxont <queenly9@icloud.com>
2025-08-06 15:23:56 +0900

    메인페이지 UI 완료

--- src/components/SelectField.jsx
+++ src/components/SelectField.jsx
@@ -1,22 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 export default function SelectField() {
-    const [selected, setSelected] = useState('');
+    const [selected, setSelected] = useState('50');
     const options = [10, 20, 30, 40, 50, 60];
 
     const handleChange = (e) => {
         setSelected(e.target.value);
     };
+
     return (
-        <>
-            <select id="number" value={selected} onChange={handleChange} className="rounded-sm border border-[#FF5B5E] w-35">
-                <option value="50" />
+        <div className="relative w-40 h-7">
+            <select
+                id="number"
+                value={selected}
+                onChange={handleChange}
+                className="appearance-none w-full h-full text-base px-2 border border-[#FF5B5E] rounded-sm focus:outline-none"
+            >
                 {options.map((num) => (
                     <option key={num} value={num}>
                         {num}
                     </option>
                 ))}
             </select>
-        </>
+
+            {/*버튼 부분*/}
+            <div className="pointer-events-none absolute top-0 right-0 h-full px-1 flex items-center rounded-sm bg-[#FF5B5E]">
+                <span className="text-white text-xs">▼</span>
+            </div>
+        </div>
     );
 }
--- src/features/Main/MainPage.jsx
+++ src/features/Main/MainPage.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SelectField from "../../components/SelectField.jsx";
-import {Input} from "react-select/animated";
 import Button from "../../components/Button.jsx";
+import tomato from "../../assets/icons/tomato.svg";
+import {useNavigate} from "react-router-dom";
 
 export default function MainPage() {
-  return (
+    let navigate = useNavigate();
+    const [repeat, setRepeat] = useState('');
+
+    const handleStart = () => {
+        if (!repeat || Number(repeat) < 1) {
+            alert("반복횟수를 1 이상으로 입력해주세요.")
+        }
+        else {
+            navigate('/timer');
+        }
+    }
+    return (
       <>
           <div className="items-center justify-items-center">
-              <p className="text-4xl font-bold mb-20">루틴 설정하기</p>
-              <div className="flex flex-row justify-between w-64 mb-5">
-                  <span>스터디 시간</span>
+              <p className="text-4xl font-bold mb-20 mt-20">루틴 설정하기</p>
+              <div className="flex flex-row justify-between w-65 mb-5">
+                  <span className="text-lg">스터디 시간</span>
                   <SelectField/>
               </div>
-              <div className="flex flex-row justify-between w-64 mb-5">
-                  <span>쉬는 시간</span>
+              <div className="flex flex-row justify-between w-65 mb-5">
+                  <span className="text-lg">쉬는 시간</span>
                   <SelectField/>
               </div>
-              <div className="flex flex-row justify-between w-64 mb-20">
-                  <span>반복 횟수</span>
-                  <input className="rounded-sm border border-[#FF5B5E] w-35" />
+              <div className="flex flex-row justify-between w-65 mb-20">
+                  <span className="text-lg">반복 횟수</span>
+                  <input
+                      type="number"
+                      min="1"
+                      value={repeat}
+                      onChange={(e) => setRepeat(e.target.value)}
+                      className="rounded-sm border border-[#FF5B5E] w-40 h-7 px-2"
+                  />
               </div>
-              <Button size='md' filled='filled' disabled={true} onClick={() => {}}>
+              <div className="items-center justify-items-center">
+                  <Button size='md' filled='filled' onClick={handleStart}>
                   시작하기
-              </Button>
+                  </Button>
+              </div>
+              <div>
+                  <img
+                      src={tomato}
+                      className="absolute bottom-5 right-10 w-20 h-20"
+                  />
+              </div>
           </div>
       </>
   );
