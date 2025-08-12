da41f8436d74ed6784643eca47653dc5ee3bdde6
Ahhhhhyeong <dkdud081@gmail.com>
2025-08-12 17:10:46 +0900

    feat: 팝업창 토마토 이미지 추가

--- src/api/userStudyRecordApi.js
+++ src/api/userStudyRecordApi.js
@@ -3,17 +3,12 @@ import { db } from '../../config/firbaseConfig';
 
 export const getUserDailyRecord = async (userUid) => {
     // 로그인한 계정에 모든 공부기록 가져오기
-    console.log(userUid);
     try {
-        const userRef = ref(db, 'users');
-        const userQuery = query(userRef, orderByChild('userUid'), equalTo(userUid));
-        const result = await get(userQuery);
-        console.log(result);
-        if(result.exists()) {
-            const userData = result.val();
-            return userData;
-        }
-        return null;
+        // 직접 경로로 접근
+        const userRef = ref(db, `users/${userUid}`);
+        const snapshot = await get(userRef);
+        const userData = snapshot.val();
+        return userData.dailyRecord;
     } catch (error) {
         console.error('Error fetching user daily record:', error);
         throw error;
