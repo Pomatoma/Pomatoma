51c5a3806456553b0d738031c31c0723d501e98c
hxont <queenly9@icloud.com>
2025-08-12 15:42:40 +0900

    fix: 모달 UI 수정

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -25,7 +25,7 @@ export default function RootLayout() {
     return (
     <div className='min-h-screen bg-gray-50'>
       <Header />
-      <main className='container mx-auto px-4 py-8'>
+        <main className='w-full px-4 py-8'>
         {isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
         <Outlet />
       </main>
