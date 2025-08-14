336ba9a708ba0194cefc8df5216853952cd44f25
hxont <queenly9@icloud.com>
2025-08-14 15:37:54 +0900

    fix: 중앙 정렬 깨짐 수정

--- src/features/Main/MainPage.jsx
+++ src/features/Main/MainPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import SelectField from "../../components/SelectField.jsx";
 import Button from "../../components/Button.jsx";
-import tomato from "../../assets/icons/tomato.svg";
 import {useNavigate} from "react-router-dom";
 import {useTimerStore} from "../../store/useTimerStore.js";
 
@@ -25,33 +24,39 @@ export default function MainPage() {
         }
     }
     return (
-      <>
-          <div className="items-center justify-items-center">
-              <p className="text-4xl font-bold mb-20 mt-20">루틴 설정하기</p>
-              <div className="flex flex-row justify-between w-65 mb-5">
-                  <span className="text-lg">스터디 시간</span>
-                  <SelectField value={studyTime} onChange={setStudyTime}/>
-              </div>
-              <div className="flex flex-row justify-between w-65 mb-5">
-                  <span className="text-lg">쉬는 시간</span>
-                  <SelectField value={breakTime} onChange={setBreakTime}/>
-              </div>
-              <div className="flex flex-row justify-between w-65 mb-20">
-                  <span className="text-lg">반복 횟수</span>
-                  <input
-                      type="number"
-                      min="1"
-                      value={cycles}
-                      onChange={(e) => setCycles(Number(e.target.value))}
-                      className="rounded-sm border border-[#FF5B5E] w-40 h-7 px-2"
-                  />
-              </div>
-              <div className="items-center justify-items-center">
-                  <Button size='md' filled='filled' onClick={handleStart}>
-                      시작하기
-                  </Button>
-              </div>
-          </div>
-      </>
+        <>
+            <div className="flex items-center justify-center px-4">
+                <div className="w-full max-w-[360px]">
+                    <p className="text-4xl font-bold mb-20 mt-25 text-center">루틴 설정하기</p>
+
+                    <div className="flex flex-row items-center justify-between w-full mb-5">
+                        <span className="text-lg">스터디 시간</span>
+                        <SelectField value={studyTime} onChange={setStudyTime}/>
+                    </div>
+
+                    <div className="flex flex-row items-center justify-between w-full mb-5">
+                        <span className="text-lg">쉬는 시간</span>
+                        <SelectField value={breakTime} onChange={setBreakTime}/>
+                    </div>
+
+                    <div className="flex flex-row items-center justify-between w-full mb-20">
+                        <span className="text-lg">반복 횟수</span>
+                        <input
+                            type="number"
+                            min="1"
+                            value={cycles}
+                            onChange={(e) => setCycles(Number(e.target.value))}
+                            className="rounded-sm border border-[#FF5B5E] w-40 h-7 px-2"
+                        />
+                    </div>
+
+                    <div className="flex justify-center">
+                        <Button size="md" filled="filled" onClick={handleStart}>
+                            시작하기
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
